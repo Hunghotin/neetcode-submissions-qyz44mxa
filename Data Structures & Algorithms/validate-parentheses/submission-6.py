eb801534ccbc3c47cf1aca98c0272e745616cae2
Huang Haotian <155733002+Hunghotin@users.noreply.github.com>
2026-07-25 00:36:46 +0800

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        match = {'(':")",'[':']','{':'}'}
+        stack = []
+
+        for i in range(len(s)):
+            if s[i] in '({[':
+                stack.append(s[i])
+            else:
+                if len(stack)==0:
+                    return False
+                else:
+                    if match[stack.pop()]!=s[i]:
+                        return False
+        if len(stack)!=0:
+            return False
+
+        return True
